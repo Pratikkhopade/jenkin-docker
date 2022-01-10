d44c4305d76c21c8aa6d4f0c8716d9406de9e4ce
pratik <pratikkhopade43@gmail.com>
2022-01-10 18:03:54 +0530

    Added java demo application files

---
+++ src/main/java/org/example/java_jenkins__in_docker/App.java
@@ -0,0 +1,18 @@
+package org.example.java_jenkins__in_docker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+	public static boolean checkIfInputIsAnEvenNumber(int i) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
---
+++ src/main/java/org/example/java_jenkins__in_docker/Main.java
@@ -0,0 +1,14 @@
+package org.example.java_jenkins__in_docker;
+
+public class Main {
+	public static void main(String[] args) {
+		 //code will go in here
+		
+		
+		System.out.println (checkIfInputIsAnEvenNumber(122));
+
+	}
+	public static boolean checkIfInputIsAnEvenNumber(int
+			number){
+			 return number % 2 == 0;
+			 }}	
